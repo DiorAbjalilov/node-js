17b3302a5f586032702563ac61ea5717f5b45245
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-16 17:15:04 +0500

    adsa

--- index.js
+++ index.js
@@ -13,21 +13,27 @@
 const fs = require("fs");
 const path = require("path");
 
-fs.mkdir(path.join(__dirname, "modules"), (err) => {
-  if (err) {
-    console.log(err);
+// fs.mkdir(path.join(__dirname, "modules"), (err) => {
+//   if (err) throw err;
+
+//   console.log("folder was created succesfully");
+//   fs.writeFile(
+//     path.join(__dirname, "modules", "path.js"),
+//     "//path module",
+//     (err) => {
+//       if (err) throw console.log(err);
+
+//       console.log("Wirite file acses");
+//     }
+//   );
+// });
+
+fs.writeFile(
+  path.join(__dirname, "modules", "nodejs.txt"),
+  "//Node JS tutorial",
+  (err) => {
+    if (err) throw console.log(err);
+
+    console.log("Wirite file acses");
   }
-
-  console.log("folder was created succesfully");
-  fs.writeFile(
-    path.join(__dirname, "modules", "path.js"),
-    "//path module",
-    (err) => {
-      if (err) throw console.log(err);
-
-      console.log("Wirite file acses");
-    }
-  );
-});
-
-console.log(fs);
+);
