67a4f24b273d817d00dae16988dd1721a7658a59
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-17 17:11:32 +0500

    asd

--- index.js
+++ index.js
@@ -63,26 +63,3 @@
 //     console.log("Name change");
 //   }
 // );
-
-const os = require("os");
-
-// Platform
-console.log(os.platform());
-
-// Architect --raziyrat
-console.log(os.arch());
-
-// Inform
-// console.log(os.cpus());
-
-// Free memory
-console.log(os.freemem());
-
-// Nechta joy qoldi
-console.log(os.totalmem());
-
-// Entry directory
-console.log(os.homedir());
-
-// time work
-console.log(os.uptime());
