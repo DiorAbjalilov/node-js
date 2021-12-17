8a1faad05da6f31813f7d333064b2ebdbc5c9afe
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-17 16:18:45 +0500

    sadas

--- index.js
+++ index.js
@@ -10,56 +10,79 @@
 
 // FS = File System
 
-const fs = require("fs");
-const path = require("path");
-const data = require("./data");
+// const fs = require("fs");
+// const path = require("path");
+// const data = require("./data");
 
-// fs.mkdir(path.join(__dirname, "modules"), (err) => {
-//   if (err) throw err;
+// // fs.mkdir(path.join(__dirname, "modules"), (err) => {
+// //   if (err) throw err;
 
-//   console.log("folder was created succesfully");
-//   fs.writeFile(
-//     path.join(__dirname, "modules", "path.js"),
-//     "//path module",
-//     (err) => {
-//       if (err) throw console.log(err);
+// //   console.log("folder was created succesfully");
+// //   fs.writeFile(
+// //     path.join(__dirname, "modules", "path.js"),
+// //     "//path module",
+// //     (err) => {
+// //       if (err) throw console.log(err);
 
-//       console.log("Wirite file acses");
-//     }
-//   );
-// });
+// //       console.log("Wirite file acses");
+// //     }
+// //   );
+// // });
 
-fs.writeFile(
-  path.join(__dirname, "modules", "nodejs.txt"),
-  "//Node JS tutorial",
-  (err) => {
-    if (err) throw console.log(err);
+// fs.writeFile(
+//   path.join(__dirname, "modules", "nodejs.txt"),
+//   "//Node JS tutorial",
+//   (err) => {
+//     if (err) throw console.log(err);
 
-    console.log("Wirite file acses");
-    fs.appendFile(
-      path.join(__dirname, "modules", "nodejs.txt"),
-      " by Diyor Abjalilov",
-      (err) => {
-        if (err) throw err;
+//     console.log("Wirite file acses");
+//     fs.appendFile(
+//       path.join(__dirname, "modules", "nodejs.txt"),
+//       " by Diyor Abjalilov",
+//       (err) => {
+//         if (err) throw err;
 
-        console.log("Documend chanched");
-        fs.readFile(
-          path.join(__dirname, "modules", "nodejs.txt"),
-          (err, data) => {
-            if (err) throw err;
-            console.log(data);
-          }
-        );
-      }
-    );
-  }
-);
+//         console.log("Documend chanched");
+//         fs.readFile(
+//           path.join(__dirname, "modules", "nodejs.txt"),
+//           (err, data) => {
+//             if (err) throw err;
+//             console.log(data);
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
 
-fs.rename(
-  path.join(__dirname, "modules", "nodejs.txt"),
-  path.join(__dirname, "modules", "node.txt"),
-  (err) => {
-    if (err) throw err;
-    console.log("Name change");
-  }
-);
+// fs.rename(
+//   path.join(__dirname, "modules", "nodejs.txt"),
+//   path.join(__dirname, "modules", "node.txt"),
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Name change");
+//   }
+// );
+
+const os = require("os");
+
+// Platform
+console.log(os.platform());
+
+// Architect --raziyrat
+console.log(os.arch());
+
+// Inform
+// console.log(os.cpus());
+
+// Free memory
+console.log(os.freemem());
+
+// Nechta joy qoldi
+console.log(os.totalmem());
+
+// Entry directory
+console.log(os.homedir());
+
+// time work
+console.log(os.uptime());
