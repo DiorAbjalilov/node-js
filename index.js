9cb3092a1c9bd600852416f8268790f520a7f5ca
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-19 01:38:25 +0500

    sad

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  // req = server so'rov
-  // res = server javob
-  res.write("<h1>Hello world Node js</h1>");
-  res.write("<h1>Hello world JavaScript</h1>");
-  res.write("<h1>Hello world React js</h1>");
-  res.end();
+  if (req.method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    if (req.url === "/") {
+      fs.readFile(
+        path.join(__dirname, "views", "index.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    } else if (req.url === "/contact") {
+      fs.readFile(
+        path.join(__dirname, "views", "contact.html"),
+        "utf-8",
+        (err, content) => {
+          if (err) throw err;
+          res.end(content);
+        }
+      );
+    }
+  } else if (req.method === "POST") {
+    const body = [];
+    res.writeHead(200, { "Content-type": "text/html; charset=utf-8" });
+    req.on("data", (data) => {
+      body.push(Buffer.from(data));
+    });
+    req.on("end", () => {
+      const message = body.toString().split("=")[1];
+      console.log(body.toString());
+      res.end(`<span>Your message: <b>${message}</b> </span>`);
+    });
+  }
 });
 
 server.listen(5000, () => {
