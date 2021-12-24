315adfb1b184a07c04dc3877bbe62e05655e0177
DiorAbjalilov <abjalilovdiyor@gmail.com>
2021-12-24 18:30:16 +0500

    asd

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const server = http.createServer((req, res) => {
     } else if (req.url === "/api/user") {
       res.writeHead(200, { "Content-type": "text/json" });
       const cars = [
-        { name: "Mers", color: "Black" },
+        { name: "Nexia", color: "Black" },
         { name: "BMW", color: "White" },
       ];
       res.end(JSON.stringify(cars));
